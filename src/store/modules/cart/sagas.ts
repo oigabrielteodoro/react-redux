a65923a390d0df7940486e36cce5cee938b69d40
oigabrielteodoro <oigabrielteodoro@gmail.com>
2020-10-08 13:27:16 -0300

    chore: get current quantity of cart

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,6 +1,18 @@
-import { all, takeLatest } from 'redux-saga/effects';
+import { all, takeLatest, select } from 'redux-saga/effects';
+import { ApplicationState } from 'store';
+import { addProductToCart } from './actions';
 
-function checkProductStock() {
+type CheckProductStockRequest = ReturnType<typeof addProductToCart>
+
+function* checkProductStock({ payload }: CheckProductStockRequest) {
+  const { product } = payload;
+
+  const currentQuantity: number = yield select((state: ApplicationState) => {
+    return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0;
+  })
+
+  console.log(currentQuantity);
+
   console.log('Adicionou ao carrinho')
 }
 
