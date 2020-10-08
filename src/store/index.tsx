6eb719f6c5a2cf22e165932be92033837e14014f
oigabrielteodoro <oigabrielteodoro@gmail.com>
2020-10-08 13:21:09 -0300

    chore: add redux-saga

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,14 +1,22 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
 
 import { CartState } from './modules/cart/types';
 
 import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
  
 export interface ApplicationState {
   cart: CartState;
 } 
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
  
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
  
+sagaMiddleware.run(rootSaga);
+
 export default store;
